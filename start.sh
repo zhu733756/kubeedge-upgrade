50f2f75a5be711d0b859fddde6abcf5250c71629
zhu733756 <qq1079333812@gmail.com>
2022-03-27 21:28:51 +0800

    refine start.sh
    Signed-off-by: zhu733756 <qq1079333812@gmail.com>

--- start.sh
+++ start.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
 KUBEEDGE_VERSION=v1.10.0
-BIN_PATH=/etc/kubeedge/upgrade/${KUBEEDGE_VERSION}/bin/edgecore
-CONFIG_PATH=/etc/kubeedge/upgrade/${KUBEEDGE_VERSION}/config/edgecore.yaml 
 LOG_PATH=/etc/kubeedge/upgrade/log/edgecore.log
 
+BIN_PATH=/etc/kubeedge/upgrade/${KUBEEDGE_VERSION}/bin/edgecore
+DEFAULT_BIN_PATH=/usr/local/bin/edgecore
+CONFIG_PATH=/etc/kubeedge/upgrade/${KUBEEDGE_VERSION}/config/edgecore.yaml
+DEFAULT_CONFIG_PATH=/etc/kubeedge/config/edgecore.yaml
+
 systemctl stop edgecore 
+
+echo "Start the fresh Edgecore..."
 chmod +x ${BIN_PATH} && nohup ${BIN_PATH} --config ${CONFIG_PATH} > ${LOG_PATH} 2>&1 &
+
+max_check_times=5
+i=1
+while (( i <= $max_check_times )) 
+do
+	health=$(curl -s http://localhost:10350/healthz)
+	if [[ "$health" == "ok" ]]; then
+		echo "The upgraded Edgecore's status is ok. Try to upgrade..."
+        	ln -srf ${BIN_PATH} ${DEFAULT_BIN_PATH}
+        	ln -srf ${CONFIG_PATH} ${DEFAULT_CONFIG_PATH}
+                pkill edgecore && systemctl enable edgecore && systemctl daemon-reload && systemctl restart edgecore && sleep 3;
+		echo "EdgeCore upgrades successfully..."
+		break
+	fi
+	sleep 3;
+done
+
+
+
--- stop.sh
+++ stop.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-KUBEEDGE_VERSION=v1.10.0
-BIN_PATH=/etc/kubeedge/upgrade/${KUBEEDGE_VERSION}/bin/edgecore
-DEFAULT_BIN_PATH=/usr/local/bin/edgecore
-CONFIG_PATH=/etc/kubeedge/upgrade/${KUBEEDGE_VERSION}/config/edgecore.yaml
-DEFAULT_CONFIG_PATH=/etc/kubeedge/config/edgecore.yaml
-
 health=$(curl -s http://localhost:10350/healthz)
-echo $health
-if [[ "$health" == "ok" ]]; then
-	ln -srf ${BIN_PATH} ${DEFAULT_BIN_PATH}
-	ln -srf ${CONFIG_PATH} ${DEFAULT_CONFIG_PATH}
+if [[ "$health" != "ok" ]]; then
+	echo "Restart Older Edgecore..."
+	pkill edgecore && systemctl enable edgecore && systemctl daemon-reload && systemctl restart edgecore
 fi
-
-echo "Restart Edgecore..."
-pkill edgecore && systemctl enable edgecore && systemctl daemon-reload && systemctl restart edgecore
 
